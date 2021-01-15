8c486e4c87f1451a8a463bb382e34c09c8fbcfbf
acetza <acetza.2021@gmail.com>
2021-01-15 07:16:32 -0600

    commit

---
+++ source/little_world.c
@@ -0,0 +1,4 @@
+
+#include "manager.h"
+
+int main(int argc, char const *argv[]) { manager_start(); }
---
+++ source/manager.c
@@ -0,0 +1,9 @@
+#include "manager.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void manager_start() {
+  printf("Hello little-world\n");
+  exit(EXIT_SUCCESS);
+}
